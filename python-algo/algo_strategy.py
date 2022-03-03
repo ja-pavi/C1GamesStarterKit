305c41f51119f193a08875bf7bb172fe4ed3a3dd
Jacob Villanueva <68416756+ja-pavi@users.noreply.github.com>
2022-03-02 19:05:24 -0600

    update

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -82,7 +82,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         if game_state.turn_number < 5:
             self.stall_with_interceptors(game_state)
         else:
-            self.remove_all()
+            self.remove_all(game_state)
             # Now let's analyze the enemy base to see where their defenses are concentrated.
             # If they have many units in the front we can build a line for our demolishers to attack them at long range.
             if self.detect_enemy_unit(game_state, unit_type=None, valid_x=None, valid_y=[14, 15]) > 10:
@@ -232,12 +232,6 @@ class AlgoStrategy(gamelib.AlgoCore):
                 gamelib.debug_write("Got scored on at: {}".format(location))
                 self.scored_on_locations.append(location)
                 gamelib.debug_write("All locations: {}".format(self.scored_on_locations))
-
-    def remove_all():
-        for index in range(HALF_ARENA):
-            y
-            if(contains_stationary_unit()):
-                remove_unit(index)
         
     def remove_all(self, game_state):
         self.enemy_defences = []
